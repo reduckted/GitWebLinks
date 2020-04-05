d33679b098b0d5f60a0809961c16ca459795b990
reduckted <reduckted@outlook.com>
2020-04-05 18:54:21 +1000

    Changed npm scripts to be usable by GitHub actions

--- test/index.ts
+++ test/index.ts
@@ -1,8 +1,38 @@
-import * as testRunner from 'vscode/lib/testrunner';
+import * as glob from 'glob';
+import * as Mocha from 'mocha';
+import * as path from 'path';
 
-testRunner.configure({
-    ui: 'bdd',
-    useColors: true
-});
+export function run(): Promise<void> {
+    let mocha: Mocha;
+    let root: string;
 
-module.exports = testRunner;
+    mocha = new Mocha({
+        ui: 'bdd',
+        useColors: true
+    });
+
+    root = __dirname;
+
+    return new Promise((resolve, reject) => {
+        glob('**/**.test.js', { cwd: root }, (err, files) => {
+            if (err) {
+                return reject(err);
+            }
+
+            files.forEach((f) => mocha.addFile(path.resolve(root, f)));
+
+            try {
+                mocha.run((failures) => {
+                    if (failures > 0) {
+                        reject(new Error(`${failures} tests failed.`));
+                    } else {
+                        resolve();
+                    }
+                });
+            } catch (err) {
+                console.error(err); // tslint:disable-line: no-console
+                reject(err);
+            }
+        });
+    });
+}
