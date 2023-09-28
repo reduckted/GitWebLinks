2e7008187881ce07e487e0dfff45cc69ee2899f8
reduckted <reduckted@outlook.com>
2023-09-28 21:03:17 +1000

    Replaced uuid with crypto.

--- vscode/test/helpers/directory.ts
+++ vscode/test/helpers/directory.ts
@@ -1,8 +1,8 @@
+import { randomUUID } from 'crypto';
 import { promises as fs } from 'fs';
 import * as os from 'os';
 import { join } from 'path';
 import { rimraf } from 'rimraf';
-import { v4 as guid } from 'uuid';
 
 import { isErrorCode } from '../../src/utilities';
 
@@ -18,7 +18,7 @@ export class Directory {
     public static async create(): Promise<Directory> {
         let dir: Directory;
 
-        dir = new Directory(join(os.tmpdir(), guid()));
+        dir = new Directory(join(os.tmpdir(), randomUUID()));
         await fs.mkdir(dir.path, { recursive: true });
 
         return dir;
